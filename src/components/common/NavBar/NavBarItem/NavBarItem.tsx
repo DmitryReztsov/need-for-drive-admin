55e0ec21eb79ccbe786c259b7eb8b22ef8f1fe82
DmitryReztsov <rez_dima@mail.ru>
2022-04-12 22:18:55 +0300

    V-08 правки

--- src/components/common/NavBar/NavBarItem/NavBarItem.tsx
+++ src/components/common/NavBar/NavBarItem/NavBarItem.tsx
@@ -6,15 +6,17 @@ interface INavBarItemProps {
   component: ReactComponentElement<any>,
   children: React.ReactNode,
   href?: string,
+  close?: () => void,
 }
 
-function NavBarItem({component, children, href}: INavBarItemProps) {
+function NavBarItem({component, children, href, close}: INavBarItemProps) {
   return (
     <Link
       sx={navBarItem}
       underline="none"
       color={'text.primary'}
       href={href || ''}
+      onClick={close}
     >
       <Box sx={navBarContent}>
         {component}
